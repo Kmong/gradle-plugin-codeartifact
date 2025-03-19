188b333a2f740d6bb261211ba8704c09f59c5410
Teddy Youn <teddy@kmong.com>
2025-03-19 15:42:18 +0900

    feat: aws credentails chains, profile, default, container, instance profile

--- src/main/kotlin/com/kmong/Credentials.kt
+++ src/main/kotlin/com/kmong/Credentials.kt
@@ -1,13 +1,17 @@
 package com.kmong
 
 import software.amazon.awssdk.auth.credentials.AwsCredentials
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain
+import software.amazon.awssdk.auth.credentials.ContainerCredentialsProvider
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider
 
 fun getAwsCredentials(profileName: String?): AwsCredentials {
-    return if (profileName != null) {
-        ProfileCredentialsProvider.create(profileName).resolveCredentials()
-    } else {
-        DefaultCredentialsProvider.create().resolveCredentials()
-    }
+    return AwsCredentialsProviderChain.builder()
+        .addCredentialsProvider(ProfileCredentialsProvider.create(profileName))
+        .addCredentialsProvider(DefaultCredentialsProvider.create())
+        .addCredentialsProvider(ContainerCredentialsProvider.create())
+        .addCredentialsProvider(InstanceProfileCredentialsProvider.create())
+        .build().resolveCredentials()
 }
